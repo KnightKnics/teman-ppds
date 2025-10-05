c8062e8a8d760be486d6e084bc06969655773861
Rian Aditia <rian.bonfilio@gmail.com>
2025-10-05 19:05:46 +0700

    Functions.js

---
+++ src/learn-js/functions.js
@@ -0,0 +1,10 @@
+function penjumlahan(input1, input2) {
+    let result = 0
+
+    result = input1 + input2
+    console.log(`hasilnya adalah ${result}`)
+    return result
+}
+
+result = penjumlahan(1, 2)
+console.log(result)
